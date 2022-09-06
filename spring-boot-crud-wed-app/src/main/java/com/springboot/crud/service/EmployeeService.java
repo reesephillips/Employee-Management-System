b7a708b2362f5f3f94d6165c92e70d5438bf959e
Reese Phillips <rjphillips485@gmail.com>
2022-09-06 11:30:09 -0400

    First commit

---
+++ spring-boot-crud-wed-app/src/main/java/com/springboot/crud/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.springboot.crud.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.springboot.crud.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	
+	Employee getEmployeeById(long id);
+	
+	void deleteEmployeeById(long id);
+	
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+
+}
